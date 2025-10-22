b4b7d685389f1bb35c9be682ca321e0712ff88d0
Erika Milevskaya <rika.gumerova@gmail.com>
2025-10-22 13:38:38 +0300

    feat: implement createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../lib');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  const letters = members
+    .filter(member => typeof member === 'string')
+    .map(name => name.trim()) 
+    .filter(name => name.length > 0)
+    .map(name => name[0].toUpperCase());
+    const result = letters.sort().join('');
+    return result;
 }
 
 module.exports = {
