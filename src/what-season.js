6c3ccc76e31e2099c26e124b37204adcfce39e68
Erika Milevskaya <rika.gumerova@gmail.com>
2025-10-22 13:13:50 +0300

    feat: implement getSeason function

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,18 @@ const { NotImplementedError } = require('../lib');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-function getSeason(/* date */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length > 0) {
+    throw new Error('Invalid date!');
+  }
+  let month = date.getMonth() + 1;
+  if (month === 12 || month === 1 || month === 2) return 'winter';
+  if (month === 3 || month === 4 || month === 5) return 'spring';
+  if (month === 6 || month === 7 || month === 8) return 'summer';
+  if (month === 9 || month === 10 || month === 11) return 'autumn';
 }
 
 module.exports = {
