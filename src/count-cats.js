2c4aae3d8f120fb4283769597780e5349cf54059
Erika Milevskaya <rika.gumerova@gmail.com>
2025-10-23 00:24:47 +0300

    feat: implement countCats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,9 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  let newMatrix =  matrix.flat().filter(item => item === '^^');
+  return newMatrix.length;  
 }
 
 module.exports = {
