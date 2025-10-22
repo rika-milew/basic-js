b27c6b7e286a91fbf8a1090e5fe82db9a5465424
Erika Milevskaya <rika.gumerova@gmail.com>
2025-10-22 12:06:03 +0300

    feat: implement getEmailDomain function

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,8 +10,9 @@ const { NotImplementedError } = require('../lib');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
+function getEmailDomain(email) {
+  const items = email.split('@');
+  return items[items.length - 1];
 }
 
 module.exports = {
